caeb52021e7086c5daa2a51cb3f0a2c67ef12365
Cole Johnstone <colejohnstone25@yahoo.com>
2023-09-20 21:57:45 -0500

    Milestone 2

--- app/src/main/java/com/cs407/lab2/CalculatorActivity.java
+++ app/src/main/java/com/cs407/lab2/CalculatorActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
     TextView textView;
@@ -14,7 +16,12 @@ public class CalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.calculator_activity);
         textView=(TextView) findViewById(R.id.textView2);
         Intent intent = getIntent();
-        String str = intent.getStringExtra("message");
-        textView.setText("Hello " + str);
+        String str = intent.getStringExtra("answer");
+        textView.setText(str);
+    }
+
+    public void goBack(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -4,27 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+import java.lang.Object;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void clickFunction(View view){
-        EditText myTextField = (EditText) findViewById(R.id.editTextText);
-        goToActivity(myTextField.getText().toString());
-        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    public void timesFunction(View view){
+        float firstNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num1)));
+        float secondNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num2)));
+        if(firstNum == 0 || secondNum == 0)
+            Toast.makeText(MainActivity.this, "Enter a number in both fields", Toast.LENGTH_LONG).show();
+        else {
+            Intent intent = new Intent(this, CalculatorActivity.class);
+            intent.putExtra("answer", String.valueOf(firstNum*secondNum));
+            startActivity(intent);
+        }
     }
-
-    public void goToActivity(String s){
-        Intent intent=new Intent(this, CalculatorActivity.class);
-        intent.putExtra("message",s);
-        startActivity(intent);
+    public void divideFunction(View view){
+        float firstNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num1)));
+        float secondNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num2)));
+        if(secondNum == 0)
+            Toast.makeText(MainActivity.this, "Cannot divide by nothing", Toast.LENGTH_LONG).show();
+        else if(firstNum == 0)
+            Toast.makeText(MainActivity.this, "Enter a number in both fields", Toast.LENGTH_LONG).show();
+        else {
+            Intent intent = new Intent(this, CalculatorActivity.class);
+            intent.putExtra("answer", String.valueOf(firstNum/secondNum));
+            startActivity(intent);
+        }
+    }
+    public void addFunction(View view){
+        float firstNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num1)));
+        float secondNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num2)));
+        if(firstNum == 0 || secondNum == 0)
+            Toast.makeText(MainActivity.this, "Enter a number in both fields", Toast.LENGTH_LONG).show();
+        else {
+            Intent intent = new Intent(this, CalculatorActivity.class);
+            intent.putExtra("answer", String.valueOf(firstNum+secondNum));
+            startActivity(intent);
+        }
+    }
+    public void subtractFunction(View view){
+        float firstNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num1)));
+        float secondNum = Integer.parseInt(checkNums((EditText) findViewById(R.id.num2)));
+        if(firstNum == 0 || secondNum == 0)
+            Toast.makeText(MainActivity.this, "Enter a number in both fields", Toast.LENGTH_LONG).show();
+        else {
+            Intent intent = new Intent(this, CalculatorActivity.class);
+            intent.putExtra("answer", String.valueOf(firstNum-secondNum));
+            startActivity(intent);
+        }
+    }
+    private String checkNums(EditText editText){
+        if(Objects.nonNull(editText) && Objects.nonNull(editText.getText()) && !editText.getText().toString().isEmpty()){
+            Log.i("INFO",String.valueOf(editText.getText().toString()));
+            return editText.getText().toString();
+        }
+        return "0";
     }
 }
