90d643e2a8204a8c40ebaebb3836ca6dc470483b
Cole Johnstone <colejohnstone25@yahoo.com>
2023-09-20 19:02:16 -0500

    Milestone 1

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -2,7 +2,11 @@ package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view){
+        EditText myTextField = (EditText) findViewById(R.id.editTextText);
+        goToActivity(myTextField.getText().toString());
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void goToActivity(String s){
+        Intent intent=new Intent(this, CalculatorActivity.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
     }
 }
